3ae31744500b5fb2166bb61b77b74bc31cff61b6
Ananasaaa <kairyakn@gmail.com>
2024-07-18 11:35:49 +0300

    modified

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ const userInfo = {
     surname : 'Evans',
     age : 18,
     address : '7, Oxford street, London',
-    tel : 441470697005,
+    tel : 4414706970568,
+    email: 'evanstest@gmail.com',
 }
 
 const entries = Object.entries(userInfo);
 entries.forEach(([key, value]) => {
     console.log(`${key}: ${value}`);
 })
+
 
 
 
